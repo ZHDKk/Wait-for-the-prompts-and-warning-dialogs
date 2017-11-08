583c21df24d96b21f30728bc00f7a78dc106f06a
ZHDKk <1083861095@qq.com>
2017-11-08 10:28:16 +0800

    第一次提交

--- 等待提示器和警告对话框/ViewController.h
+++ 等待提示器和警告对话框/ViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIAlertViewDelegate>
+{
+    //定义警告对话框视图对象
+    UIAlertView *_alertView;
+    
+    //等待提示对象
+    //当下载，或加载比较大的文件，可以显示此控件，处于提示等待状态
+    UIActivityIndicatorView *_activityIndicator;
+}
 
+@property(retain,nonatomic) UIAlertView *_alertView;
+@property(retain,nonatomic) UIActivityIndicatorView *_acitivtyIndicator;
 
 @end
 
--- 等待提示器和警告对话框/ViewController.m
+++ 等待提示器和警告对话框/ViewController.m
@@ -13,13 +13,61 @@
 @end
 
 @implementation ViewController
+@synthesize _acitivtyIndicator=_activityIndicator;
+@synthesize  _alertView= _alertView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    for(int i=0;i<2;i++){
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        btn.frame = CGRectMake(100, 100+100*i, 100, 40);
+        if (i==0) {
+            [btn setTitle:@"警告提示框" forState:UIControlStateNormal];
+        }
+        else if (i==1){
+            [btn setTitle:@"等待提示框" forState:UIControlStateNormal];
+        }
+        btn.tag = 101+i;
+        
+        [btn addTarget:self action:@selector(pressBtn:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.view addSubview:btn];
+        
+    }
 }
 
+-(void) pressBtn:(UIButton*) btn{
+    if (btn.tag == 101) {
+        _alertView = [[UIAlertView alloc] initWithTitle:@"警告" message:@"你的手机电量过低，即将关机，请保存好数据" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"ok",@"11",nil];
+        [_alertView show];
+    }
+    else if(btn.tag == 102){
+        _activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(100, 300, 80, 80)];
+        _activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;  //设定提示风格：灰、白、大白
+        [self.view addSubview:_activityIndicator];
+        
+        //启动动画并显示
+        [_activityIndicator startAnimating];
+        
+        
+    }
+}
+//点击对话框的按钮时，调用此函数
+//参数一：对话框对象本身。  参数二：按钮索引
+-(void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"index = %ld\n",buttonIndex);
+}
 
+-(void) alertView:(UIAlertView *)alertView willDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"对话框即将消失");
+}
+-(void) alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"对话框已经消失");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
